c20c994203928fbbe75d2b4b0b7ef2f43893476d
Adriano Santana <mano1die2mili@gmail.com>
2020-04-01 16:14:43 -0300

    Novas atualizações

--- curso js udemy/aula21/index.js
+++ curso js udemy/aula21/index.js
@@ -1,1 +1,15 @@
- console.log(false || false);
+ /*
+operadores lógicos
+&& -> AND -> E -> todas as expressões precisam se verdadeiras para retornar true
+|| -> OR -> OU ->  
+! -> NOT -> NÂO 
+ */
+
+//const expressaoOr = false || false;
+//console.log(expressaoOr);
+
+const usuario = 'Adriano';
+const senha = '123456';
+
+const vaiLogar = usuario === 'Adriano' && senha === '123456';
+console.log(vaiLogar);
---
+++ curso js udemy/aula22/index.js
@@ -0,0 +1,17 @@
+/*
+Entre 0 e 11 horas o sistema fala bom dia
+Entre 12 e 17 horas o sistema fala boa tarde
+Entre 18 e 23 horas o sistema fala boa noite
+*/
+
+const hora = 34;
+
+if (hora >= 0 && hora <= 11){
+    console.log('Bom dia');
+    }else if (hora >= 12 && hora <= 17){
+        console.log('Boa tarde');
+    }else if (hora >= 18 && hora <= 23){
+        console.log('Boa noite');
+    }else {
+        console.log('Hora não identificada');
+    }
